b1f562918eba7159fa08acdb27c892f8d50ff5f7
lucaxgomex <lucas.g.oliv.dev@gmail.com>
2020-12-30 20:19:46 -0300

    Adição da primeira atividade.

---
+++ ifpi-poo-2020.2/Semana01/TestaRetangulo.java
@@ -0,0 +1,13 @@
+public class TestaRetangulo {
+  /*
+  public int area() {
+    return base * altura;
+  }
+  */
+
+  public static void main(String[] args) {
+    Retangulo obj = new Retangulo(2, 4);
+    
+    System.out.println("Valor da area: " + area());
+  }
+}
