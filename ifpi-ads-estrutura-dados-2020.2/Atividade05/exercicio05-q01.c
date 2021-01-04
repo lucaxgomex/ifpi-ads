de5884ef19d90111f3a755ae7602d2bb5f4cecd5
lucaxgomex <lucas.g.oliv.dev@gmail.com>
2021-01-04 02:28:06 -0300

    Reparando erro na estrutura que calcula o tamanho do array.

--- ifpi-ads-estrutura-dados-2020.2/Atividade05/exercicio05-q01.c
+++ ifpi-ads-estrutura-dados-2020.2/Atividade05/exercicio05-q01.c
@@ -1,53 +1,38 @@
+//Inversao da frase e troca das consoantes por '#'
 #include <stdlib.h>
 #include <stdio.h>
-#include <locale.h>
 #include <string.h>
 
-int main(void)
-{
+void reverse(char getArray[50]) {
+    char inverted[50];
+    int size = strlen(getArray), k = 0;
 
-    setlocale(LC_ALL, "");
-
-    char nome[30], caractere, recebe[30], invertida[30];
-    // char vogal[] = {'a', 'e', 'i', 'o', 'u'};
-
-    printf("Nome: ");
-    fgets(nome, 30, stdin);
-
-    // scanf("%s", &nome);
-
-    int tam = strlen(nome);
-    printf("Tamanho: %d\n", tam);
-    //
-    // printf("%c", nome[0]);
-    printf("\n");
-    for (int i = 0; i < tam; i++)
-    {
-        caractere = nome[i];
-        if (caractere == 'a' || caractere == 'e' || caractere == 'i' || caractere == 'o' || caractere == 'u')
-        {
-            recebe[i] = caractere;
-        }
-        else
-        {
-            recebe[i] = '#';
-        }
-    }
-
-    // Inverter
-    int tamRecebida;
-    int k = 0;
-
-    tamRecebida = strlen(recebe);
-
-    for (int i = tamRecebida - 1; i >= 0; i--)
-    {
-        invertida[k] = recebe[i];
+    for (int i = size - 1; i >= 0; i--) {
+        inverted[k] = getArray[i];
         k++;
     }
 
-    printf("%s", invertida);
+    printf("\n%s\n", inverted);
+}
 
+int main() {
+    char name[50], consonant, getArray[50];
+
+    printf("Nome: ");
+    fgets(name, 30, stdin); //Recebe uma string
+
+    printf("Tamanho: %ld\n", strlen(name) - 1);
+
+    for (int i = 0; i < strlen(name) - 1; i++) {
+
+        consonant = name[i];
+        if (consonant == 'a' || consonant == 'e' || consonant == 'i' || consonant == 'o' || consonant == 'u') {
+            getArray[i] = consonant;
+        } else {
+            getArray[i] = '#';
+        }
+    }
+
+    reverse(getArray);
     return 0;
 }
-// Não tenho certeza se está correta.
