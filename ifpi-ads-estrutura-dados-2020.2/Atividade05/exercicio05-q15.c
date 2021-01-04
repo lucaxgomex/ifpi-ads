f519de03d172501f1134360c0944ea15a182c4ee
lucaxgomex <lucas.g.oliv.dev@gmail.com>
2021-01-04 05:00:52 -0300

    Remoção das vogais.

--- ifpi-ads-estrutura-dados-2020.2/Atividade05/exercicio05-q15.c
+++ ifpi-ads-estrutura-dados-2020.2/Atividade05/exercicio05-q15.c
@@ -1,34 +1,29 @@
+//Remoção de vogais
 #include <stdlib.h>
 #include <stdio.h>
-#include <locale.h>
 #include <string.h>
 
-int main(void) {
+int main() {
+    char quote[100];
+    char vowels[] = {'a', 'e', 'i', 'o', 'u'};
+    char noVowel[100];
+    int i, j, size = 0;
 
-    char frase[100];
-    char vogais[] = {'a', 'e', 'i', 'o', 'u'};
-    char fraseSemVogal[100];
-    int i, j, tamFrase=0;
+    printf("Digite algo: ");
+    fflush(stdin);
+    fgets(quote, 100, stdin);
 
-    printf("Frase: ");
-    // fflush(stdin);
-    gets(frase);
-
-    tamFrase = strlen(frase);
+    size = strlen(quote);
     
-    for ( i = 0; i < tamFrase; i++)
-    {
-        for (j = 0; j < 5; j++)
-        {
-            if (frase[i] != vogais[j])
-            {
-                fraseSemVogal[i] = frase[i];
+    for (i = 0; i < size; i ++) {
+        for (j = 0; j < 5; j ++) {
+            if (quote[i] != vowels[j]) {
+                noVowel[i] = quote[i];
+            } else {
+                noVowel[i] = ' ';
             }
         }
     }
-    printf("%s", fraseSemVogal);
-    
-    
+    printf("\n%s", noVowel);
     return 0;
-
 }
